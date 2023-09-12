bdc292f11715e0585cf761bc805809b5adb541f9
Aldrean Ponsoy <141804661+AldreanPonsoy@users.noreply.github.com>
2023-09-12 16:10:18 +0800

    first commit

---
+++ main.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="navbar-container container">
+            <input type="checkbox" name="" id="">
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+            <ul class="menu-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="aboutme.php">About Me</a></li>
+                <li><a href="profile.php ">Personal Info</a></li>
+                <li><a href="#">My Work</a></li>
+            </ul>
+            <h1 class="logo">A</h1>
+        </div>
+
+        <div class="containerinfo">
+            <div class="contentBox">
+                <div class="content">
+                    <h3>Aldrean G. Ponsoy</h3>
+                    <p>Bachelor Of industrial Technology Major In Computer Engineering Technology</p>
+                    <a href="#">Aldreanponsoy11@gmail.com</a>
+                    <a href="">Resume</a>  
+                </div>
+            </div>
+            <div class="contentBox">
+                <h1>header</h1>
+            </div>
+
+            <div class="contentBox">
+                <h2>scroll-3</h2>
+            </div>
+            <div class="contentBox">
+                <h2>scroll-4</h2>
+            </div>
+            <div class="contentBox">
+                <h2>scroll-5</h2>
+            </div>
+        </div>
+
+    </nav>
+
+    <footer class="navbarbottom fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+      <img class="navbar-brand" src="css/pquelogo.png">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://app.bploparanaque.com/home/">BPLO Paranaque City Hall<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </footer> 
+<script src="js/scrollanimation.js"></script>
+
+        <div class="containername">
+            <div>
+                <h3>Aldrean G. Ponsoy</h3>
+                <p>Bachelor Of industrial Technology Major In Computer Engineering Technology</p>
+                <a href="#">Aldreanponsoy11@gmail.com</a>
+                <a href="">Resume</a>  
+            </div>
+            
+        </div>
+
+    </nav>
+
+</body>
+
+</html>
